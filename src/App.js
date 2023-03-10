442fb96304e879f52d6847f9f1d538c2f660ebaa
Iman Khodor <iman.khodor20@gmail.com>
2023-03-10 15:41:14 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>version2</h1>
+      <h1 className="title">first app react</h1>
+      <p>start now !</p>
+    </div>
+  );
+}
+
+export default App;
